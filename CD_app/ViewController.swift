b41612a95c1b055b5b79553137962ca2b9ba59c4
Sean Zhang <mpb15sz@seans-macbook-pro.local>
2016-11-14 18:04:02 -0500

    Initial Commit

---
+++ CD_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CD_app
+//
+//  Created by Sean Zhang on 11/14/16.
+//  Copyright © 2016 Sean Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
